206e02acd239046e774c2e4208470974c41bc812
NicolasCalvi <nicolascalvi717@gmail.com>
2023-09-15 12:31:27 -0300

    peticion get probada y andando

--- src/Handlers/countriesHandler.js
+++ src/Handlers/countriesHandler.js
@@ -1,10 +1,12 @@
-const getCountriesHandler = (req, res) => {
+const { getAllCountries } = require("../controllers/countriesController");
+
+const getCountriesHandler = async (req, res) => {
   const { name } = req.query;
   try {
-    if (name) res.status(200).send(`Ruta donde cada objeto es un país con su información en este caso ${name} `);
-    else {
-      res.status(200).send('todos los usuarios');
-    }
+    const countries = name
+      ? await getAllCountries({ name })
+      : await getAllCountries();
+    res.status(200).json(countries);
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
@@ -13,7 +15,11 @@ const getCountriesHandler = (req, res) => {
 const getCountriesIdHandler = (req, res) => {
   const { id } = req.params;
   try {
-    res.status(200).send(`Ruta donde obtiene el detalle de un país especifico en este caso : ${id}`);
+    res
+      .status(200)
+      .send(
+        `Ruta donde obtiene el detalle de un país especifico en este caso : ${id}`
+      );
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
